f906aa086327e734aef97d548509ff1099a68327
Edgar <edgar.lazaro23@gmail.com>
2025-03-19 10:56:30 -0500

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,35 @@
-
-import path from 'path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@store': path.resolve(__dirname, './src/store'),
-      '@config': path.resolve(__dirname, './src/config'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
-    }
+      "@": path.resolve(__dirname, "./src"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@config": path.resolve(__dirname, "./src/config"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/styles/themes/_variables.scss" as *;`
-      }
-    }
+        additionalData: `@use "@/styles/themes/_variables.scss" as *;`,
+      },
+    },
   },
   build: {
     lib: {
       entry: "src/widget.tsx",
       name: "TaxDataWidget",
       fileName: "tax-data-widget",
-      formats: ["umd"]
-    }
-  }
-})
+      formats: ["umd"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
+});
